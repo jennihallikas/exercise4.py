6bbe602b608ede9212280ca5e5a6f2b50d12c6c4
Jenni <jenni.hallikas@metropolia.fi>
2022-08-25 15:25:48 +0300

    exercise

---
+++ main.py
@@ -0,0 +1,24 @@
+# phase 1
+import math
+import random
+
+#name = input("What is your name? ")
+#print("Hello, " + name + "!")
+
+# phase 2
+#radius = float(input("Give the radius? "))
+#print(f"Area is {math.pi * radius ** 2:10.1f}")
+
+# phase 6
+#print(f"{random.randint(0, 999):03d}")
+#print(str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)))
+
+lenght = float(input("Give me the lenght? "))
+width = float(input("Give me the width? "))
+perimeter = lenght * 2 + width * 2
+area = lenght * width
+print("Perimeter is " + str(perimeter))
+print("Area is " + str(area))
+
+
+
